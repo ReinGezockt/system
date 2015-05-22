f72679338c392306d7e63a268c1cae60399fdb95
ReinGezockt <reinhard.schuster@icloud.com>
2015-05-22 18:51:38 +0200

    Bis jetz

--- src/Commands/BukkitBroadcast.java
+++ src/Commands/BukkitBroadcast.java
@@ -1,6 +1,8 @@
 package Commands;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 /**
  * Created by Reinhard Schuster on 22.05.2015.
@@ -8,5 +10,12 @@ import org.bukkit.command.CommandExecutor;
 public class BukkitBroadcast implements CommandExecutor{
 
 
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
 
+
+
+
+    }
 }
